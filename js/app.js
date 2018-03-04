dbe8b9026f3091a157b105810d2461ede2be06e8
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-05 00:03:41 +0530

    download attribute

--- js/app.js
+++ js/app.js
@@ -7,4 +7,10 @@ const $secureLinks = $('a[href^="https://"]');
 // This will select all the elements with a .pdf in the end 
 const $pdfs = $('a[href$=".pdf"]');
 
-$pdfs.hide();
+// $pdfs.hide();
+
+// When your site has 25, 50 or maybe 100 links and you want to open them in a new tab
+$secureLinks.attr('target', '_blank');
+
+// Add download attribute, Now our file will be downloaded instead of opening in the browser. Adding download attribute is bit tricky because it doesn't need any value. 
+$pdfs.attr('download', true);
